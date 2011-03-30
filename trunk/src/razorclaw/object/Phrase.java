ab155de822c272895c493a5172365e813ac0b10c
me@yuan-shuai.info <me@yuan-shuai.info@4746290d-42a9-02db-a286-0d23cafa71f6>
2011-03-30 22:16:47 +0000

    added datastore for phrases, local cache PhraseProperty

--- trunk/src/razorclaw/object/Phrase.java
+++ trunk/src/razorclaw/object/Phrase.java
@@ -1,6 +1,7 @@
 package razorclaw.object;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -12,183 +13,61 @@ import java.util.List;
  */
 public class Phrase implements Comparable<Phrase> {
 
-    private boolean _isCaseSentitive = false;
+	private boolean _isCaseSentitive = false;
 
-    private int _occurance = 1;
-
-    private double _tf = 0.0, _idf = 0.0, _tf_idf = 0.0;
-
-    private String _pos;
-
-    private boolean _inTitle = false, _inKeywords = false,
-	    _inDescription = false;
-
-    public Phrase(String s) {
-	_phrase = s;
-    }
-    
-    public Phrase(){
+	private String _phrase;
 	
-    }
+	private ArrayList<PhraseProperty> _properties;
+	
+	private String _language, _code;
 
-    /*
-     * public String getPhrase() { StringBuilder phrase = new StringBuilder();
-     * for (Word word : _words) { phrase.append(word.getWord() + " "); }
-     * 
-     * return phrase.toString().substring(0, phrase.length() - 1); }
-     */
-
-    /*
-     * public List<? extends Word> getWords() { return _words; }
-     */
-
-    /*
-     * public int getNoWords() { return _words.size(); }
-     */
-
-    @Override
-    public int hashCode() {
-	return getPhrase().hashCode();
-    }
-
-    @Override
-    public boolean equals(Object object) {
-	Phrase altPhrase = (Phrase) object;
-	if (_isCaseSentitive)
-	    return altPhrase.getPhrase().equals(getPhrase());
-	return altPhrase.getPhrase().equalsIgnoreCase(getPhrase());
-    }
-
-    private String _phrase;
-
-    public String toString() {
-	DecimalFormat f = new DecimalFormat("#.####");
-
-	StringBuilder sb = new StringBuilder();
-	sb.append(_phrase);
-	sb.append(": ");
-	sb.append(_occurance);
-	sb.append(", ");
-	sb.append(f.format(_tf));
-	sb.append(", ");
-	sb.append(_pos);
-	sb.append(", ");
-	sb.append(_inTitle);
-	sb.append(", ");
-	sb.append(_inKeywords);
-	sb.append(", ");
-	sb.append(_inDescription);
-
-	return sb.toString();
-    }
-
-    // ----------------------comparator--------------------------
-    @Override
-    public int compareTo(Phrase o) {
-	//
-	return getPhrase().compareTo(o.getPhrase());
-
-    }
-
-    public static class ByTF implements Comparator<Phrase> {
-	@Override
-	public int compare(Phrase arg0, Phrase arg1) {
-	    return (arg0.getTF() > arg1.getTF()) ? 1 : 0;
+	public Phrase(String s) {
+		_phrase = s;
 	}
-    }
 
-    public static class ByIDF implements Comparator<Phrase> {
-	@Override
-	public int compare(Phrase arg0, Phrase arg1) {
-	    return (arg0.getIDF() > arg1.getIDF()) ? 1 : 0;
+	public Phrase() {
+
 	}
-    }
 
-    public static class ByTF_IDF implements Comparator<Phrase> {
 	@Override
-	public int compare(Phrase arg0, Phrase arg1) {
-	    return (arg0.getTF_IDF() > arg1.getTF_IDF()) ? 1 : 0;
+	public int hashCode() {
+		return getPhrase().hashCode();
 	}
-    }
 
-    // ----------------------getter and setter--------------------
-    public void setPhrase(String _phrase) {
-	this._phrase = _phrase;
-    }
+	@Override
+	public boolean equals(Object object) {
+		Phrase altPhrase = (Phrase) object;
+		if (_isCaseSentitive)
+			return altPhrase.getPhrase().equals(getPhrase());
+		return altPhrase.getPhrase().equalsIgnoreCase(getPhrase());
+	}
 
-    public String getPhrase() {
-	return _phrase;
-    }
+	// ----------------------comparator--------------------------
+	@Override
+	public int compareTo(Phrase o) {
+		//
+		return getPhrase().compareTo(o.getPhrase());
 
-    public int getOccurance() {
-	return _occurance;
-    }
+	}
 
-    public void setTF(double _tf) {
-	this._tf = _tf;
-    }
+	// ----------------------getter and setter--------------------
+	public void setPhrase(String _phrase) {
+		this._phrase = _phrase;
+	}
 
-    public double getTF() {
-	return _tf;
-    }
+	public String getPhrase() {
+		return _phrase;
+	}
 
-    public void setIDF(double _idf) {
-	this._idf = _idf;
-    }
+	public void setCaseSensitive(boolean isCaseSentitive) {
+		_isCaseSentitive = isCaseSentitive;
+	}
 
-    public double getIDF() {
-	return _idf;
-    }
+	public void setProperties(ArrayList<PhraseProperty> _properties) {
+		this._properties = _properties;
+	}
 
-    public void setTF_IDF(double _tf_idf) {
-	this._tf_idf = _tf_idf;
-    }
-
-    public double getTF_IDF() {
-	return _tf_idf;
-    }
-
-    public void setOccurance(int _occurance) {
-	this._occurance = _occurance;
-    }
-
-    public void increaseOccurance() {
-	this._occurance++;
-    }
-
-    public void setPOS(String _pos) {
-	this._pos = _pos;
-    }
-
-    public String getPOS() {
-	return _pos;
-    }
-
-    public void setInTitle(boolean _inTitle) {
-	this._inTitle = _inTitle;
-    }
-
-    public boolean isInTitle() {
-	return _inTitle;
-    }
-
-    public void setInKeywords(boolean _inKeywords) {
-	this._inKeywords = _inKeywords;
-    }
-
-    public boolean isInKeywords() {
-	return _inKeywords;
-    }
-
-    public void setInDescription(boolean _inDescription) {
-	this._inDescription = _inDescription;
-    }
-
-    public boolean isInDescription() {
-	return _inDescription;
-    }
-
-    public void setCaseSensitive(boolean isCaseSentitive) {
-	_isCaseSentitive = isCaseSentitive;
-    }
+	public ArrayList<PhraseProperty> getProperties() {
+		return _properties;
+	}
 }
