94ea8aa2f6710839f539ef563965f885c2ed6720
me@yuan-shuai.info <me@yuan-shuai.info@4746290d-42a9-02db-a286-0d23cafa71f6>
2011-03-30 22:16:47 +0000

    added datastore for phrases, local cache PhraseProperty

--- src/razorclaw/datastore/PharseStoreHandler.java
+++ src/razorclaw/datastore/PharseStoreHandler.java
@@ -2,11 +2,14 @@ package razorclaw.datastore;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 import com.google.appengine.api.datastore.*;
 
 import razorclaw.object.Phrase;
+import razorclaw.object.PhraseProperty;
+import razorclaw.object.Dictionaries.PartOfSpeech;
 
 /**
  * save or load phrase entities from datastore
@@ -15,46 +18,111 @@ import razorclaw.object.Phrase;
  * 
  */
 public class PharseStoreHandler {
-    private String _language;
 
-    private String _encode;
+	private static HashMap<String, PhraseProperty> _cache;
 
-    private static ArrayList<Phrase> _cache;
+	private static DatastoreService _datastore = DatastoreServiceFactory
+			.getDatastoreService();
 
-    private Date _lastSync;
+	public static void put(String phrase, PhraseProperty pp) {
+		if (_cache != null) {
 
-    private static DatastoreService _datastore = DatastoreServiceFactory
-	    .getDatastoreService();
-
-    public static void put(Phrase phrase) {
-
-    }
-
-    public static Phrase get(String phrase) {
-
-    }
-
-    private static void save() {
-
-    }
-
-    private static void load() {
-	_cache = new ArrayList<Phrase>();
-
-	Query query = new Query("PhraseProperty");
-	List<Entity> result = _datastore.prepare(query).asList(
-		FetchOptions.Builder.withDefaults());
-	if (result != null) {
-	    Phrase p = 
-	    for(Entity e : result){
-		
-	}
-	    
+		}
 	}
 
-    }
+	public static PhraseProperty get(String phrase) {
 
-    private static void saveToDS(Phrase phrase) {
+	}
 
-    }
+	private static void save() {
+
+	}
+
+	private static void load() {
+		_cache = new ArrayList<Phrase>();
+
+		Query query = new Query("PhraseProperty");
+		List<Entity> result = _datastore.prepare(query).asList(
+				FetchOptions.Builder.withDefaults());
+		if (result != null) {
+			Phrase p = null;
+			PhraseProperty pp = null;
+
+			for (Entity e : result) {
+				if (p == null) {
+					p = new Phrase(e.getParent().getName()); // phrase
+
+				} else if (p.getPhrase().equals(e.getParent().getName())) {
+
+				} else if (!p.getPhrase().equals(e.getParent().getName())) {
+					_cache.add(p);
+
+					p = new Phrase(e.getParent().getName()); // phrase
+
+				}
+
+				pp = new PhraseProperty();
+				// properties
+				pp.setForwardURL((String) e.getProperty("ForwardURL"));
+
+				pp.setOccurance((Integer) e.getProperty("Occurance"));
+				pp.setTFScore((Double) e.getProperty("TF"));
+				pp.setTFIDFScore((Double) e.getProperty("TFIDF"));
+				pp.setBM25FScore((Double) e.getProperty("BM25F"));
+				pp.setLanguageModelScore((Double) e
+						.getProperty("LanguageModel"));
+
+				pp.setTitle((Boolean) e.getProperty("InTitle"));
+				pp.setMetaKeywords((Boolean) e.getProperty("InMetaKeywords"));
+				pp.setMetaDescription((Boolean) e
+						.getProperty("InMetaDescription"));
+				pp.setH1((Boolean) e.getProperty("InH1"));
+				pp.setH2((Boolean) e.getProperty("InH2"));
+
+				pp.setPartOfSpeech(PartOfSpeech.load((String) e
+						.getProperty("PartOfSpeech")));
+
+				pp.setNew(false);
+
+				p.getProperties().add(pp);
+			}
+			_cache.add(p);
+		}
+
+	}
+
+	/**
+	 * save a new PhraseProperty entity to datastore
+	 * 
+	 * NOTE: duplicate check is ignored
+	 * 
+	 * @param phrase
+	 * @param pp
+	 */
+	private static void putToDS(String phrase, PhraseProperty pp) {
+		Entity phraseEntity = new Entity("Phrase", phrase);
+		phraseEntity.setProperty("phrase", phrase);
+
+		Entity propertyEntity = new Entity("PhraseProperty",
+				phraseEntity.getKey());
+		propertyEntity.setProperty("ForwardURL", pp.getForwardURL());
+		propertyEntity.setProperty("PartOfSpeech", pp.getPartOfSpeech());
+
+		propertyEntity.setProperty("Occurance", pp.getOccurance());
+		propertyEntity.setProperty("TF", pp.getTFScore());
+		propertyEntity.setProperty("IDF", pp.getIDFScore());
+		propertyEntity.setProperty("TFIDF", pp.getTFIDFScore());
+		propertyEntity.setProperty("BM25F", pp.getBM25FScore());
+		propertyEntity.setProperty("LanguageModelScore",
+				pp.getLanguageModelScore());
+
+		propertyEntity.setProperty("InTitle", pp.isTitle());
+		propertyEntity.setProperty("InMetaKeywords", pp.isMetaKeywords());
+		propertyEntity.setProperty("InMetaDescription", pp.isMetaDescription());
+		propertyEntity.setProperty("InH1", pp.isH1());
+		propertyEntity.setProperty("InH2", pp.isH2());
+
+		_datastore.put(phraseEntity);
+		_datastore.put(propertyEntity);
+	}
 }
---
+++ src/razorclaw/object/PhraseProperty.java
@@ -0,0 +1,148 @@
+package razorclaw.object;
+
+import razorclaw.object.Dictionaries.PartOfSpeech;
+
+/**
+ * holds properties for a phrases in a given document(forwardURL)
+ * 
+ * @author Shuai Yuan
+ *
+ */
+public class PhraseProperty {
+	private String _forwardURL;
+	
+	private int _occurance;
+	
+	private double _tfScore, _idfScore, _tfidfScore, _bm25fScore, _languageModelScore;
+	
+	private boolean _isTitle, _isH1, _isH2, _isMetaKeywords, _isMetaDescription, _isCapital;
+	
+	private PartOfSpeech _partOfSpeech;
+	
+	/**
+	 * flag for sync
+	 */
+	private boolean _isNew;
+
+	public void setForwardURL(String _forwardURL) {
+		this._forwardURL = _forwardURL;
+	}
+
+	public String getForwardURL() {
+		return _forwardURL;
+	}
+
+	public void setOccurance(int _occurance) {
+		this._occurance = _occurance;
+	}
+
+	public int getOccurance() {
+		return _occurance;
+	}
+
+	public void setTFScore(double _tfScore) {
+		this._tfScore = _tfScore;
+	}
+
+	public double getTFScore() {
+		return _tfScore;
+	}
+
+	public void setIDFScore(double _idfScore) {
+		this._idfScore = _idfScore;
+	}
+
+	public double getIDFScore() {
+		return _idfScore;
+	}
+
+	public void setTFIDFScore(double _tfidfScore) {
+		this._tfidfScore = _tfidfScore;
+	}
+
+	public double getTFIDFScore() {
+		return _tfidfScore;
+	}
+
+	public void setBM25FScore(double _bm25fScore) {
+		this._bm25fScore = _bm25fScore;
+	}
+
+	public double getBM25FScore() {
+		return _bm25fScore;
+	}
+
+	public void setLanguageModelScore(double _languageModelScore) {
+		this._languageModelScore = _languageModelScore;
+	}
+
+	public double getLanguageModelScore() {
+		return _languageModelScore;
+	}
+
+	public void setMetaDescription(boolean _isMetaDescription) {
+		this._isMetaDescription = _isMetaDescription;
+	}
+
+	public boolean isMetaDescription() {
+		return _isMetaDescription;
+	}
+
+	public void setMetaKeywords(boolean _isMetaKeywords) {
+		this._isMetaKeywords = _isMetaKeywords;
+	}
+
+	public boolean isMetaKeywords() {
+		return _isMetaKeywords;
+	}
+
+	public void setH2(boolean _isH2) {
+		this._isH2 = _isH2;
+	}
+
+	public boolean isH2() {
+		return _isH2;
+	}
+
+	public void setH1(boolean _isH1) {
+		this._isH1 = _isH1;
+	}
+
+	public boolean isH1() {
+		return _isH1;
+	}
+
+	public void setTitle(boolean _isTitle) {
+		this._isTitle = _isTitle;
+	}
+
+	public boolean isTitle() {
+		return _isTitle;
+	}
+
+	public void setCapital(boolean _isCapital) {
+		this._isCapital = _isCapital;
+	}
+
+	public boolean isCapital() {
+		return _isCapital;
+	}
+
+	public void setPartOfSpeech(PartOfSpeech _partOfSpeech) {
+		this._partOfSpeech = _partOfSpeech;
+	}
+
+	public PartOfSpeech getPartOfSpeech() {
+		return _partOfSpeech;
+	}
+
+	public void setNew(boolean _isNew) {
+		this._isNew = _isNew;
+	}
+
+	public boolean isNew() {
+		return _isNew;
+	}
+	
+	
+}
