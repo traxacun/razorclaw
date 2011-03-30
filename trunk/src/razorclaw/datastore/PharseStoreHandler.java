834e35b0cd68ebc1725754054713c1508d209f9c
me@yuan-shuai.info <me@yuan-shuai.info@4746290d-42a9-02db-a286-0d23cafa71f6>
2011-03-30 16:02:20 +0000

    daily commit

--- trunk/src/razorclaw/datastore/PharseStoreHandler.java
+++ trunk/src/razorclaw/datastore/PharseStoreHandler.java
@@ -2,6 +2,9 @@ package razorclaw.datastore;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import com.google.appengine.api.datastore.*;
 
 import razorclaw.object.Phrase;
 
@@ -9,16 +12,49 @@ import razorclaw.object.Phrase;
  * save or load phrase entities from datastore
  * 
  * @author Shuai YUAN
- *
+ * 
  */
 public class PharseStoreHandler {
     private String _language;
-    
+
     private String _encode;
-    
-    private ArrayList<Phrase> _phrases;
-    
+
+    private static ArrayList<Phrase> _cache;
+
     private Date _lastSync;
-    
-    
+
+    private static DatastoreService _datastore = DatastoreServiceFactory
+	    .getDatastoreService();
+
+    public static void put(Phrase phrase) {
+
+    }
+
+    public static Phrase get(String phrase) {
+
+    }
+
+    private static void save() {
+
+    }
+
+    private static void load() {
+	_cache = new ArrayList<Phrase>();
+
+	Query query = new Query("PhraseProperty");
+	List<Entity> result = _datastore.prepare(query).asList(
+		FetchOptions.Builder.withDefaults());
+	if (result != null) {
+	    Phrase p = 
+	    for(Entity e : result){
+		
+	}
+	    
+	}
+
+    }
+
+    private static void saveToDS(Phrase phrase) {
+
+    }
 }
